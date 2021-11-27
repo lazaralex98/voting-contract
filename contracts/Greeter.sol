fa2830afeb882ee8bf0568dbf984b915db8ad524
lazaralex98 <alex2012ro@gmail.com>
2021-11-27 16:38:55 +0200

    first commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,49 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+
+contract VotingContract {
+  struct Proposal {
+    string name;   
+    string description;
+    uint voteCount; 
+  }
+
+  Proposal[] public proposals;
+
+  constructor(string memory _proposal_name, string memory _proposal_description) {
+    console.log("Deploying a VotingContract with first proposal:", _proposal_name);
+    proposals.push(Proposal({
+      name: _proposal_name,
+      description : _proposal_description,
+      voteCount: 0
+    }));
+  }
+
+  function getProposals() public view returns (Proposal[] memory) {
+    console.log("Getting all proposals");
+    return proposals;
+  }
+
+  function getProposal(uint _index) public view returns (Proposal memory) {
+    console.log("Getting proposal with index of: '%s'", _index);
+    return proposals[_index];
+  }
+
+  function setProposal(string memory _proposal_name, string memory _proposal_description) public {
+    console.log("Proposing video subject: '%s'", _proposal_name);
+    proposals.push(Proposal({
+      name: _proposal_name,
+      description : _proposal_description,
+      voteCount: 0
+    }));
+  }
+
+  function voteProposal(uint _index) public {
+    console.log("Proposal of index: '%s' has '%s' vote(s). Voting for it.", _index, proposals[_index].voteCount);
+    proposals[_index].voteCount += 1;
+    console.log("Vote cast! Now proposal of index: '%s' has '%s' vote(s).", _index, proposals[_index].voteCount);
+  }
+}
---
+++ test/sample-test.js
@@ -0,0 +1,66 @@
+const { expect } = require("chai");
+
+describe("Deploy & Vote", function () {
+  it("Should return the proposal with 1 vote after voting", async function () {
+    const VotingContract = await ethers.getContractFactory("VotingContract");
+    const votingContract = await VotingContract.deploy(
+      "Web3 Vlog",
+      "You should make a vlog of you learning web3."
+    );
+
+    await votingContract.deployed();
+
+    const proposalBeforeVote = await votingContract.getProposal(0);
+    expect(proposalBeforeVote).to.be.include(
+      "Web3 Vlog",
+      "You should make a vlog of you learning web3.",
+      { _hex: ethers.utils.hexlify(0), _isBigNumber: true }
+    );
+
+    await votingContract.voteProposal(0);
+    const proposalAfterVote = await votingContract.getProposal(0);
+    expect(proposalAfterVote).to.be.include(
+      "Web3 Vlog",
+      "You should make a vlog of you learning web3.",
+      { _hex: ethers.utils.hexlify(1), _isBigNumber: true }
+    );
+  });
+});
+
+describe("Deploy & Propose", function () {
+  it("Should return the new list of proposals after submitting one", async function () {
+    const VotingContract = await ethers.getContractFactory("VotingContract");
+    const votingContract = await VotingContract.deploy(
+      "Web3 Vlog",
+      "You should make a vlog of you learning web3."
+    );
+
+    await votingContract.deployed();
+
+    const allProposalsBeforeProposal = await votingContract.getProposals();
+    expect(allProposalsBeforeProposal).to.be.include(
+      "Web3 Vlog",
+      "You should make a vlog of you learning web3.",
+      { _hex: ethers.utils.hexlify(0), _isBigNumber: true }
+    );
+
+    await votingContract.setProposal(
+      "Landing Page Reviews",
+      "We want more landing page reviews."
+    );
+
+    const allProposalsAfterProposal = await votingContract.getProposals();
+    expect(allProposalsAfterProposal).to.include(
+      [
+        "Web3 Vlog",
+        "You should make a vlog of you learning web3.",
+        { _hex: ethers.utils.hexlify(0), _isBigNumber: true },
+      ],
+      [
+        "Landing Page Reviews",
+        "We want more landing page reviews.",
+        { _hex: ethers.utils.hexlify(0), _isBigNumber: true },
+      ]
+    );
+  });
+});
