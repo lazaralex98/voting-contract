1e922d7d4932003c3c1ed031350f44a79482a0b1
lazaralex98 <alex2012ro@gmail.com>
2021-12-22 14:54:35 +0200

    made a loader that appears when voting

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { ethers } from "ethers";
 import votingAbi from "../utils/votingAbi.json";
 import { Disclosure } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/outline";
 declare let window: any;
 
 const currentProposals = [
@@ -29,6 +31,7 @@ export default function Home() {
 
   const [account, setAccount] = useState("");
   const [allProposals, setAllProposals] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     getAllProposals();
@@ -111,6 +114,7 @@ export default function Home() {
    */
   async function vote(id: number): Promise<boolean> {
     try {
+      setLoading(true);
       const { ethereum } = window;
       if (!ethereum) {
         console.warn("Make sure you have MetaMask connected");
@@ -134,16 +138,17 @@ export default function Home() {
       );
       console.log("Sending vote...", voteTxn.hash);
 
-      // TODO make a loader while the user waits
       // TODO BUG the transaction fails
       await voteTxn.wait();
 
       console.log("Successfully voted for ", id);
 
+      setLoading(false);
       getAllProposals();
       return true;
     } catch (error) {
       console.error("Error:", error);
+      setLoading(false);
       return false;
     }
   }
@@ -346,6 +351,86 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* Loading modal */}
+      <Transition.Root show={loading} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed z-10 inset-0 overflow-y-auto"
+          onClose={setLoading}
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="hidden sm:inline-block sm:align-middle sm:h-screen"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+                <div>
+                  <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
+                    <svg
+                      className="animate-spin h-5 w-5 text-blue-700"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-5">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg leading-6 font-medium text-gray-900"
+                    >
+                      Loading...
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Please wait while we proccess your request.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <p className="text-gray-500">
